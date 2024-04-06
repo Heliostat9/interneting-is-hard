b6fc829bb599e1f821c490da1b068775e80a1ebf
Хитрин Артём <heliostat@MacBook-Air-Hitrin.local>
2024-04-06 18:51:24 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+0 Interneting is hard course
---
+++ script.js
@@ -0,0 +1,4 @@
+let el = document.getElementById("par");
+        el.addEventListener("click", function(event) {
+        el.innerHTML = "Fuck yourself and go work!";
+})
